5cc8d6806bf2628010110a6410bcaa858f7ffadc
Bharathkumar1522 <bk152002@gmail.com>
2025-03-07 16:41:19 +0530

    changes in testimonial section

--- src/components/TestimonialSections.jsx
+++ src/components/TestimonialSections.jsx
@@ -4,7 +4,7 @@ import Testimonial from "./Testimonial.jsx";
 const testimonials = [
     {
         name: "Raghavendra Royal",
-        position: "Top Customer",
+        position: "Customer",
         company: "TastyDen",
         image: "https://res.cloudinary.com/dyecmgvcy/image/upload/v1741264409/malepic_yb0vf4.jpg",
         text: "Nice place to eat tasty food and also very nice ambience and all items are at affordable prices",
@@ -12,7 +12,7 @@ const testimonials = [
     },
     {
         name: "Bharath kumar",
-        position: "Regular Customer",
+        position: "Customer",
         company: "TastyDen",
         image: "https://res.cloudinary.com/dyecmgvcy/image/upload/v1741264409/malepic_yb0vf4.jpg",
         text: "Best wraps and friend chicken in puttur. Must try",
@@ -20,7 +20,7 @@ const testimonials = [
     },
     {
         name: "Nasirkhan",
-        position: "Regular Customer",
+        position: "Customer",
         company: "TasyDen",
         image: "https://res.cloudinary.com/dyecmgvcy/image/upload/v1741264409/malepic_yb0vf4.jpg",
         text: "If you are looking for best eats in puttur this is the place",
@@ -31,7 +31,7 @@ const testimonials = [
     const TestimonialsSection = () => {
     return (
         <>
-        <h2 className="text-white text-2xl sm:text-4xl font-bold text-center mt-20 mb-15">What Our Clients Are Saying</h2>
+        <h2 className="text-white text-2xl sm:text-4xl font-bold text-center mt-20 mb-15"> Customer Ratings</h2>
         <div className="testimonials-section">
         <div className="testimonial-cards">
             {testimonials.map((testimonial, index) => (
